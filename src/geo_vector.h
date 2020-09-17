a04fff3d168171a3ab613817574814350bca4ba3
Romain Francois <romain@rstudio.com>
2020-09-17 14:04:58 +0200

    make geo_vector purerly virtual

--- src/geo_vector.h
+++ src/geo_vector.h
@@ -35,15 +35,17 @@ public:
   geo_vector() {}
   geo_vector(std::vector<T> content) : _storage(content) {}
 
+  virtual ~geo_vector() {};
+
   size_t size() const { return _storage.size(); }
   T operator[](size_t i) const { return _storage[i]; }
   size_t dimensions() const {
     return dim;
   };
-  virtual cpp11::writable::strings dim_names() const;
+  virtual cpp11::writable::strings dim_names() const = 0;
 
   // Conversion
-  virtual cpp11::writable::doubles_matrix as_numeric() const;
+  virtual cpp11::writable::doubles_matrix as_numeric() const = 0;
 
   // Predicates
   cpp11::writable::logicals is_degenerate() const {
